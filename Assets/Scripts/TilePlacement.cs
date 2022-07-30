21aed56f2b9ea272f840e7abb013e42aa738ef00
Craig Anderson <cranderson0110@gmail.com>
2022-07-30 19:37:32 +1200

    Cycle through tiles when placing, fix placement checking

--- Assets/Scripts/TilePlacement.cs
+++ Assets/Scripts/TilePlacement.cs
@@ -5,10 +5,25 @@ using UnityEngine.Tilemaps;
 public class TilePlacement : MonoBehaviour {
 
     private Tilemap tilemap;
-    public TileBase placement;
+    public TileBase sheep;
+    public TileBase wood;
+    public TileBase bricks;
+    public TileBase stone;
+    public TileBase wheat;
+    public TileBase desert;
+
+    private TileBase[] tiles = new TileBase[6];
+
+    private int current = 0;
 
     void Start () {
         tilemap = GetComponent<Tilemap>();
+        tiles[0] = sheep;
+        tiles[1] = wood;
+        tiles[2] = bricks;
+        tiles[3] = stone;
+        tiles[4] = wheat;
+        tiles[5] = desert;
     }
 
     void Update () {
@@ -18,9 +33,13 @@ public class TilePlacement : MonoBehaviour {
         Vector3Int cellPosition = tilemap.WorldToCell(mouse);
 
         if (Input.GetButtonUp("Fire1")) {
-            if (-9 < cellPosition.x && cellPosition.x < 9) {
+            int rightLimit = Mathf.Abs(cellPosition.y % 2) == 1 ? 8 : 9;
+            if (-9 < cellPosition.x && cellPosition.x < rightLimit) {
                 if (-6 < cellPosition.y && cellPosition.y < 16) {
-                    tilemap.SetTile(cellPosition, placement);
+                    if (tilemap.GetTile(cellPosition) == null) {
+                        tilemap.SetTile(cellPosition, tiles[current++]);
+                        if (current == 6) current = 0;
+                    }
                 }
             }
         }
