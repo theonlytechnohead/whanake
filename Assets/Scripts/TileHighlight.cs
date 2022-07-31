4dfcb485cdcea0541f9e2c787926e6c10d2d7f8a
Craig Anderson <cranderson0110@gmail.com>
2022-07-31 15:37:24 +1200

    Fix touchscreen navigation highlight
    Untested though...

--- Assets/Scripts/TileHighlight.cs
+++ Assets/Scripts/TileHighlight.cs
@@ -10,20 +10,46 @@ public class TileHighlight : MonoBehaviour {
 
     private Vector3Int? previous;
 
+    private static bool requireTouch = false;
+
     void Start () {
         tilemap = GetComponent<Tilemap>();
     }
 
     void Update () {
+        if (requireTouch) {
+            if (0 < Input.touchCount) {
+                DoHighlight();
+            } else {
+                ClearHighlight();
+            }
+        } else {
+            DoHighlight();
+        }
+    }
+
+    Vector3Int GetCellUnderMouse () {
         Vector3 mouse = Input.mousePosition;
         mouse = Camera.main.ScreenToWorldPoint(mouse);
         mouse.z = 0f;
-        Vector3Int cellPosition = tilemap.WorldToCell(mouse);
+        return tilemap.WorldToCell(mouse);
+    }
+
+    void DoHighlight () {
+        Vector3Int cellPosition = GetCellUnderMouse();
         if (cellPosition != previous) {
             if (TilePlacement.legalPlacement(cellPosition)) tilemap.SetTile(cellPosition, legalHighlight);
             else tilemap.SetTile(cellPosition, illegalHighlight);
             if (previous.HasValue) tilemap.SetTile(previous.Value, null);
             previous = cellPosition;
         }
+    }
+
+    void ClearHighlight () {
+        tilemap.SetTile(GetCellUnderMouse(), null);
+    }
+
+    public static void OnlyHighlightWithTouch (bool value) {
+        requireTouch = value;
     }
 }
