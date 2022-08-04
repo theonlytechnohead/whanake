c511fa2a1eeabe83f840eef2d7896c4729598a26
Craig Anderson <cranderson0110@gmail.com>
2022-08-04 17:28:09 +1200

    Roads! Can be previewed
    And they align and I've already got the logic for picking the right one! wow, such progress

--- Assets/RoadPlacement.cs
+++ Assets/RoadPlacement.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class RoadPlacement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+[RequireComponent(typeof(Tilemap))]
+public class RoadPlacement : MonoBehaviour {
+    private Tilemap tilemap;
+
+    public Tile vertical;
+    public Tile slopeUp;
+    public Tile slopeDown;
+
+    Vector3Int previous;
+
+    void Start () {
+        tilemap = GetComponent<Tilemap>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+        Vector3 mouse = Input.mousePosition;
+        Vector3Int cell = tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(mouse));
+        cell.z = 0;
+        if (previous != cell) {
+            tilemap.SetTile(previous, null);
+            previous = cell;
+            Tile road = null;
+            if (Mathf.Abs(cell.y) % 2 == 0) {
+                // vertical
+                if (Mathf.Abs(cell.y) % 4 == 2) {
+                    if (Mathf.Abs(cell.x) % 2 == 0) {
+                        road = vertical;
+                    }
+                } else if (Mathf.Abs(cell.y) % 4 == 0) {
+                    if (Mathf.Abs(cell.x) % 2 == 1) {
+                        road = vertical;
+                    }
+                }
+            } else {
+                // slope
+                if (Mathf.Abs(cell.y) % 4 == 3) {
+                    if (Mathf.Abs(cell.x) % 2 == 0) {
+                        road = slopeUp;
+                    } else {
+                        road = slopeDown;
+                    }
+                } else if (Mathf.Abs(cell.y) % 4 == 1) {
+                    if (Mathf.Abs(cell.x) % 2 == 0) {
+                        road = slopeDown;
+                    } else {
+                        road = slopeUp;
+                    }
+                }
+            }
+            tilemap.SetTile(cell, road);
+        }
     }
 }
